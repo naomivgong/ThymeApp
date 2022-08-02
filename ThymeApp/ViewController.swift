5cdb9e99114ed80739e43388d31da4ed09e98681
Naomi Gong <naomigong2005@gmail.com>
2022-08-02 13:22:17 -0700

    First

--- ThymeApp/ViewController.swift
+++ ThymeApp/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var l: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
